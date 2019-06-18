a7ee47d82efd648e807137a86da13dff0e249fcc
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-06-18 15:29:37 -0400

    found iframe, extracting XML

--- app/controllers/ibm_infos_controller.rb
+++ app/controllers/ibm_infos_controller.rb
@@ -17,8 +17,6 @@ class IbmInfosController < ApplicationController
         @info.password = params[:ibm_info][:password]
         @info.report_name = params[:ibm_info][:report_name]
 
-        p @info
-
         b = Watir::Browser.new(:chrome)
         ibm_login_url = 'https://www.ibm.com/account/reg/us-en/login?formid=urx-34710'
         b.goto(ibm_login_url)
@@ -32,10 +30,19 @@ class IbmInfosController < ApplicationController
         b.button(type: 'submit').click
         sleep 5
 
-        b.div(title: '‪sample product report‬').click
-        sleep 20
-        r_html = b.html
-        @info.report_xml = Nokogiri::HTML.parse(r_html)
+        #Go to the report
+        b.div(title: '‪sample product report‬').click #need to figure out why the variable isn't working
+        
+        #Switch to the correct iframe where the report document is housed
+        b.driver.switch_to.frame('rsIFrameManager_1') # Switch to numerical method to prevent brittleness
+
+        #Scrape the entire XML of this page of the report
+        page_one = doc = Nokogiri::HTML(b.html)
+
+        #is it a bar chart?
+        if doc.xpath("//*[@class=\"element-shape bundle-shape\"]")
+            bar_chart_one = doc.xpath("//*[@class=\"element-shape bundle-shape\"]")
+        end
         b.close
 
         if @info.save
